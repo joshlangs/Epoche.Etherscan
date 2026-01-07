fca0cd4906f584667470c49bd2b24568f558e967
Josh <josh@josh.ca>
2026-01-07 11:12:54 -0800

    binance, arbitrum

--- Epoche.Etherscan/KnownEndpoints.cs
+++ Epoche.Etherscan/KnownEndpoints.cs
@@ -2,8 +2,8 @@
 public static class KnownEndpoints
 {
     public const string Ethereum = "https://api.etherscan.io/v2/api?chainid=1";
-    public const string BinanceSmartChain = "https://api.bscscan.com/api";
+    public const string BinanceSmartChain = "https://api.etherscan.io/v2/api?chainid=56";
     public const string Fantom = "https://api.ftmscan.com/api";
     public const string Avalanche = "https://api.snowtrace.io/api";
-    public const string Arbitrum = "https://api.arbiscan.io/api";
+    public const string Arbitrum = "https://api.etherscan.io/v2/api?chainid=42161";
 }
