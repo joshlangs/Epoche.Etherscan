e20035ef3eb9d11fc6a88f93cf792e1e5ebb4ea4
Josh <josh@josh.ca>
2026-01-08 10:02:16 -0800

    sonic/fantom

--- Epoche.Etherscan/KnownEndpoints.cs
+++ Epoche.Etherscan/KnownEndpoints.cs
@@ -3,7 +3,7 @@ public static class KnownEndpoints
 {
     public const string Ethereum = "https://api.etherscan.io/v2/api?chainid=1";
     public const string BinanceSmartChain = "https://api.etherscan.io/v2/api?chainid=56";
-    public const string Fantom = "https://api.ftmscan.com/api";
+    public const string SonicFantom = "https://api.etherscan.io/v2/api?chainid=146";
     public const string Avalanche = "https://api.snowtrace.io/api";
     public const string Arbitrum = "https://api.etherscan.io/v2/api?chainid=42161";
 }
